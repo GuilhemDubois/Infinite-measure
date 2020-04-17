496f31d17c027b22a6cf3334a16ad9c1e402ea07
Kynorii <philippe.saraiva@isep.fr>
2020-04-17 16:31:22 +0200

    question

---
+++ question.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Foire aux questions</title>
+    <link rel="stylesheet" href="headerStyle.css"/>
+    <link rel="stylesheet" href="footerStyle.css"/>
+    <link rel="stylesheet" href="question.css"/>
+</head>
+
+
+<body>
+<?php include ('header.php'); ?>
+
+<?php include ('footer.php'); ?>
+
+<div id="formulaire">
+    <form method="post">
+        <p>
+            <input type="text" name="question" id="question" placeholder="Votre question">
+            <input type="submit" name="envoyer" id="envoyer">
+        </p>
+
+    </form>
+
+</div>
+</body>
+</html>
