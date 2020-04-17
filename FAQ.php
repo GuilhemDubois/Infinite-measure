88e5002fcd64d850e356b777e3bfce5a4243734e
Kynorii <philippe.saraiva@isap.fr>
2020-04-17 15:36:20 +0200

    Ajout de la FAQ
    FAQ php et css

---
+++ FAQ.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Foire aux questions</title>
+    <link rel="stylesheet" href="headerStyle.css"/>
+    <link rel="stylesheet" href="footerStyle.css"/>
+    <link rel="stylesheet" href="FAQ.css"/>
+</head>
+
+
+<body>
+    <?php include ('header.php'); ?>
+
+    <?php include ('footer.php'); ?>
+
+<div id="formulaire">
+        <form method="post">
+            <p>
+                <input type="text" name="question" id="question" placeholder="Votre question">
+            </p>
+
+        </form>
+
+    </div>
+</body>
+</html>
