a5b729fab22433a29d323b07d9ac5ffd98ed40a0
GuilhemDubois <61823279+GuilhemDubois@users.noreply.github.com>
2020-04-23 16:39:20 +0200

    dd

--- header.php
+++ header.php
@@ -33,9 +33,7 @@ $_SESSION["location"] = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];?>
                 <a href="Resultats.php" class="m-link"><i class="fas fa-tachometer-alt"></i> Vos résultat</a>
                 <a href="NousConnaitre.php" class="m-link"><i class="far fa-question-circle"></i> A propos</a>
                 <a href="VotreProfil.php" class="m-link"><i class="fas fa-user"></i> Mon profil</a>
-                <form action="changelangue.php" method="post">
-                <button type="submit"  class="button-language">Langues</button>
-                </form>
+                <a href="changelangue.php" class="m-link"><i class="fas fa-user"></i> Langues</a>
             </div>
             <?php echo $_SESSION['langue']; ?>
 
